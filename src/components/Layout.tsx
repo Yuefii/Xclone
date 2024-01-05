a7eeb542fd47a66da32c02be64a7031dc070f5d1
yuefii <muffidahmadiawan31@gmail.com>
2024-01-05 22:32:14 +0700

    refactor code on production beta

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-
-import FollowBar from "@/components/layout/SidebarFollow"
-import Sidebar from "@/components/layout/Sidebar"
+import React from "react";
+import Sidebar from "./layouts/Sidebar";
+import SidebarFollow from "./layouts/SidebarFollow";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
@@ -9,21 +8,22 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <div className="container h-full mx-auto xl:px-30 max-w-8xl">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
-          <div 
+          <div
             className="
               col-span-3 
               lg:col-span-2 
               border-x-2
               border-sky-600
               rounded-xl
-          ">
+          "
+          >
             {children}
           </div>
-          <FollowBar />
+          <SidebarFollow />
         </div>
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
